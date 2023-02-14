ff86569fc141eb664822acbbfdc22ebc6747ac89
shui <syui56@icloud.com>
2023-02-14 19:45:06 +0900

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CalculateAveragePersonalData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.currentTimeLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // currentTimeLabel
+            // 
+            this.currentTimeLabel.AutoSize = true;
+            this.currentTimeLabel.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.currentTimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.currentTimeLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.currentTimeLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentTimeLabel.Location = new System.Drawing.Point(704, 0);
+            this.currentTimeLabel.Name = "currentTimeLabel";
+            this.currentTimeLabel.Size = new System.Drawing.Size(96, 25);
+            this.currentTimeLabel.TabIndex = 0;
+            this.currentTimeLabel.Text = "00:00:00";
+            this.currentTimeLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.currentTimeLabel.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 305);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(698, 299);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.currentTimeLabel);
+            this.Name = "Form1";
+            this.Text = "CAPD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label currentTimeLabel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CalculateAveragePersonalData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();       
+        }
+        
+        /// <summary>
+        /// データテーブルの作成,
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = createData();
+            dataGridView1.DataSource = dt;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DateTime datetime_now = DateTime.Now;
+            currentTimeLabel.Text = datetime_now.ToLongTimeString();
+        }
+
+        private DataTable createData()
+        {
+            DataTable dt = new DataTable();
+      
+            dt.Columns.Add("Day", typeof(string));
+            dt.Columns.Add("No.", typeof(int));
+            dt.Columns.Add("Required", typeof(Decimal));
+            dt.Columns.Add("wake up", typeof(string));
+
+            string fileName = "PersonalData.csv";
+            StreamReader sr = new StreamReader(@fileName);
+
+            List<string[]> lists = new List<string[]>();
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+
+                //カンマで配列の要素として分ける
+                string[] values = line.Split(',');
+
+                // 配列からリストに格納する
+                dt.Rows.Add(values);
+            }
+
+            return dt;
+        }
+    }
+}
